ac958b2ad216649e7312f524a781328724443a6a
Carsten Block <mail@carstenblock.org>
2011-01-07 16:22:59 +0100

    groupId property removed as this leads to plugin resolution problems when used in combination with other plugins

--- PowertacStyleGrailsPlugin.groovy
+++ PowertacStyleGrailsPlugin.groovy
@@ -1,5 +1,4 @@
 class PowertacStyleGrailsPlugin {
-  def groupId = 'org.powertac.plugins'
   // the plugin version
   def version = "0.1.1"
   // the version or versions of Grails the plugin is designed for
